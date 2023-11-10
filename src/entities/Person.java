f6ef42961eb1b83976064201962f4d0b70976746
Gabriel Silva Torres <gabrielst328@gmail.com>
2023-11-10 19:06:14 -0300

    Construída classe Person

--- src/entities/Person.java
+++ src/entities/Person.java
@@ -1,5 +1,28 @@
 package entities;
 
 public abstract class Person {
+	
+	protected String name;
+	protected Double annualIncome;
+	
+	public Person() {
+		
+	}
+
+	public Person(String name, Double annualIncome) {
+		super();
+		this.name = name;
+		this.annualIncome = annualIncome;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Double getAnnualIncome() {
+		return annualIncome;
+	}
+	
+	public abstract Double calcTax();
 
 }
