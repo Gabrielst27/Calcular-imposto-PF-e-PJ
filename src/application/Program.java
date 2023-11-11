6bd7d030ce10a613c7cac9c02ff071773166538a
Gabriel Silva Torres <gabrielst328@gmail.com>
2023-11-11 14:51:47 -0300

    Criada lista payers e laço de instanciação

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,29 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Person;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<Person> payers = new ArrayList<>();
+		
+		System.out.print("Enter the number of tax payers: ");
+		int n = sc.nextInt();
+		
+		for(int i = 1; i <= n; i++) {
+			
+		}
+		
+		sc.close();
 
 	}
 
