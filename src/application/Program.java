6181c5b37c91ff35bfc45d93a0bd55f201e345ea
Gabriel Silva Torres <gabrielst328@gmail.com>
2023-11-11 15:01:44 -0300

    Criada instanciação NaturalPerson

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.NaturalPerson;
 import entities.Person;
 
 public class Program {
@@ -20,7 +21,21 @@ public class Program {
 		int n = sc.nextInt();
 		
 		for(int i = 1; i <= n; i++) {
+			sc.nextLine();
+			System.out.println("Tax payer #" + i + " data:");
+			System.out.print("Individual or company (i/c): ");
+			String type = sc.nextLine();
+			System.out.print("Name: ");
+			String name = sc.nextLine();
+			System.out.print("Annual income: ");
+			double annualIncome = sc.nextDouble();
 			
+			if(type.toUpperCase().equals("I")) {
+				System.out.print("Health expendictures: ");
+				double healthSpending = sc.nextDouble();
+				
+				payers.add(new NaturalPerson(name, annualIncome, healthSpending));
+			}
 		}
 		
 		sc.close();
